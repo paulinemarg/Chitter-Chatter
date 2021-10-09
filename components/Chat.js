d7101fc4d1586b39d3be8048ff61e18d9451a268
paulinemarg <paulinemargossian@gmail.com>
2021-10-09 09:14:00 +0200

    imported mapView & CustomActions

--- components/Chat.js
+++ components/Chat.js
@@ -3,6 +3,8 @@ import { Alert, View, Platform, KeyboardAvoidingView, LogBox } from 'react-nativ
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from '@react-native-community/netinfo';
+import MapView from 'react-native-maps';
+import CustomActions from './CustomActions';
 
 const firebase = require('firebase');
 require('firebase/firestore');
@@ -19,7 +21,10 @@ class Chat extends Component {
         avatar: '',
       },
       isConnected: false,
+      image: null,
+      location: null,
     };
+
     // Firebase Config
     const firebaseConfig = {
       apiKey: "AIzaSyAVko3ajbTNrBJ7AOdqOwJqYTOpmY3UTpg",
@@ -48,8 +53,9 @@ class Chat extends Component {
   }
 
   componentDidMount() {
-    let { name } = this.props.route.params;
-
+    let name = this.props.route.params.name;
+    // Displays the user’s name in the navigation bar at the top of the chat screen.
+    this.props.navigation.setOptions({ title: name });
     // Check user connection
     NetInfo.fetch().then((connection) => {
       if (connection.isConnected) {
@@ -87,7 +93,7 @@ class Chat extends Component {
         this.setState({ isConnected: false });
         this.getMessages();
         Alert.alert(
-          'No internet connection detected! Unable to send messages.'
+          'No internet connection detected | Unable to send messages'
         );
       }
     });
@@ -105,12 +111,18 @@ class Chat extends Component {
     const messages = [];
     // Iterate through each document
     querySnapshot.forEach((doc) => {
-      let data = doc.data(); // Grabs QueryDocumentSnapshot's data
+      let data = doc.data();
       messages.push({
         _id: data._id,
         text: data.text,
         createdAt: data.createdAt.toDate(),
-        user: data.user,
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar,
+        },
+        image: data.image || '',
+        location: data.location || null,
       });
     });
     this.setState({ messages });
@@ -157,6 +169,8 @@ class Chat extends Component {
       createdAt: message.createdAt,
       text: message.text || '',
       user: message.user,
+      image: message.image || '',
+      location: message.location || null,
     });
   }
 
@@ -198,6 +212,35 @@ class Chat extends Component {
     );
   }
 
+  // Returns a MapView that shows user's location
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          showsUserLocation={true}
+          style={{
+            width: 150,
+            height: 100,
+            borderRadius: 15,
+            margin: 5,
+          }}
+          region={{
+            longitude: Number(currentMessage.location.longitude),
+            latitude: Number(currentMessage.location.latitude),
+            longitudeDelta: 0.0421,
+            latitudeDelta: 0.0922,
+          }}
+        />
+      );
+    }
+    return null;
+  }
+
+  renderActions = (props) => {
+    return <CustomActions {...props} />;
+  };
+
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: this.props.route.params.colorSelectionBackground }}>
@@ -205,8 +248,9 @@ class Chat extends Component {
           renderBubble={this.renderBubble.bind(this)}
           renderInputToolbar={this.renderInputToolbar.bind(this)}
           renderUsernameOnMessage={true}
+          renderCustomView={this.renderCustomView}
+          renderActions={this.renderActions}
           messages={this.state.messages}
-          showAvatarForEveryMessage={true}
           onSend={(messages) => this.onSend(messages)}
           user={{
             _id: this.state.uid,
